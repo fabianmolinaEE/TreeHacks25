a77a7e5f304a3c355fda24cea633c2b5a7338de7
Suprad Parashar <supradparashar@Suprads-MacBook-Pro.local>
2025-02-15 10:30:34 -0800

    Modifies the Groq Pipeline function to include model and the client in the parameters.

--- scrapybara_openai_pipeline.py
+++ scrapybara_openai_pipeline.py
@@ -1,8 +1,7 @@
 from groq import Groq
 import os
 
-def get_response(payload, chat = []):
-    print("BEFORE:", chat)
+def get_response(client, payload, chat = [], model="mixtral-8x7b-32768"):
     message = {
         "role": "user",
         "content": payload
@@ -10,21 +9,20 @@ def get_response(payload, chat = []):
     chat.append(message)
     chat_response = client.chat.completions.create(
         messages=chat,
-        model="mixtral-8x7b-32768"
+        model=model
     )
     response = chat_response.choices[0].message.content
     chat.append({
         "role": "assistant",
         "content": response
     })
-    print("AFTER:", chat)
     return response
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY"),
-)
+# client = Groq(
+#     api_key=os.getenv("GROQ_API_KEY"),
+# )
 
-print(get_response("What is the weather like today?"))
-print(get_response("What is the weather like tomorrow?"))
-print(get_response("What is the weather like in New York?"))
-print(get_response("What is the weather like in San Francisco?"))
+# print(get_response("What is the weather like today?"))
+# print(get_response("What is the weather like tomorrow?"))
+# print(get_response("What is the weather like in New York?"))
+# print(get_response("What is the weather like in San Francisco?"))
